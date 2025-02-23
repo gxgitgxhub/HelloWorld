7503432d4c5688de5e6b5edf13bc1cdf64d96c7c
Yann Mythos <mythos0mi@outlook.com>
2025-02-24 00:34:24 +0800

    第一个Java程序注释补充
    Signed-off-by: Yann Mythos <mythos0mi@outlook.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+//class用来定义一个类，public表示这个类是公开的，public、class都是Java的关键字，必须小写，
+//Main是类的名字，按照习惯，首字母H要大写。而花括号{}中间则是类的定义。
+ublic class Main { //表示一个公开的静态方法，void是方法的返回类型，而花括号{}中间的就是方法的代码
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!"); //用来打印一个字符串到屏幕上。打印print line的缩写。
     }
 }
