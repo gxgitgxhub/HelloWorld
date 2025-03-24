b9bf8a185c040904b76eaa214a003f96f545ad32
geng xi <gx00932107@techmahindra.com>
2025-03-24 09:22:18 +0800

    .

---
+++ src/com/example/helloworld/Point.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+public final class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return this.x;
+    }
+
+    public int y() {
+        return this.y;
+    }
+}
